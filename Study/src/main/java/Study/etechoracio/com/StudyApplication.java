7a6ee9df9a8c82b414a9d07ac757f4e70e208349
Vitor <vitorsouzalibrelonrestini@gmail.com>
2024-08-20 11:07:01 -0300

    a

---
+++ Study/src/main/java/Study/etechoracio/com/StudyApplication.java
@@ -0,0 +1,13 @@
+package Study.etechoracio.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplication.class, args);
+	}
+
+}
---
+++ Study/src/test/java/Study/etechoracio/com/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package Study.etechoracio.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
